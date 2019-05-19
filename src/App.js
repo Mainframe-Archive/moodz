c1290297d7470ca8f521dff381a1da0c73ae4ec6
Shane Howley <shane@mainframe.com>
2019-05-19 18:42:24 +0100

    Add basic app logic for reading/writing mood

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 // @flow
 
 import React, { Component } from 'react'
-import MainframeSDK from '@mainframe/sdk'
-import { Text } from '@morpheus-ui/core'
+import MainframeSDK, { type Contact } from '@mainframe/sdk'
+import { Text, Row, Column, Button } from '@morpheus-ui/core'
+import { Picker } from 'emoji-mart'
 
+import 'emoji-mart/css/emoji-mart.css'
 import './App.css'
 
 type Props = {}
 
-type State = {}
+type State = {
+  contacts?: Array<Contact>,
+}
 
 export default class App extends Component<Props, State> {
   sdk: MainframeSDK
@@ -16,9 +20,108 @@ export default class App extends Component<Props, State> {
   constructor() {
     super()
     this.sdk = new MainframeSDK()
+    this.state = { contacts: [] }
+  }
+
+  async componentDidMount() {
+    const contacts = await this.sdk.contacts.getApprovedContacts()
+    contacts.forEach(c => this.fetchMood(c))
+    this.setState({
+      contacts,
+    })
+  }
+
+  async fetchMood(contact) {
+    const keys = await this.sdk.comms.getSubscribable(contact.id)
+    if (!keys.includes('mood')) {
+      setTimeout(() => this.fetchMood(contact), 10000)
+      return
+    }
+
+    const observable = await this.sdk.comms.subscribe(contact.id, 'mood')
+    observable.subscribe(({ mood, timestamp }) => {
+      contact.mood = mood
+      contact.timestamp = timestamp
+      this.forceUpdate()
+    })
+  }
+
+  selectContact = async () => {
+    const contact = await this.sdk.contacts.selectContact()
+    if (
+      !contact ||
+      this.state.contacts.find(c => {
+        return c.id === contact.id
+      })
+    ) {
+      return
+    }
+
+    this.state.contacts.push(contact)
+    await this.fetchMood(contact)
+    this.forceUpdate()
+  }
+
+  setMood = async mood => {
+    const timestamp = new Date().toUTCString()
+    this.setState({ myMood: mood })
+    await Promise.all(
+      this.state.contacts.map(c => {
+        return this.sdk.comms.publish(c.id, 'mood', { mood, timestamp })
+      }),
+    )
+  }
+
+  renderMood(name, mood, timestamp) {
+    return (
+      <Row size={3}>
+        <Column>
+          <Text>{name}</Text>
+        </Column>
+        <Column>
+          <Text>{mood || '?'}</Text>
+        </Column>
+        <Column>
+          <Text>{timestamp || 'never'}</Text>
+        </Column>
+      </Row>
+    )
   }
 
   render() {
-    return <Text>It works!</Text>
+    return (
+      <div>
+        <Row size={3}>
+          <Column>
+            <Text>Name</Text>
+          </Column>
+          <Column>
+            <Text>Current Mood</Text>
+          </Column>
+          <Column>
+            <Text>Last Update</Text>
+          </Column>
+        </Row>
+        {this.renderMood('Me', this.state.myMood, ' ')}
+        {this.state.contacts &&
+          this.state.contacts.map(c =>
+            this.renderMood(c.data.profile.name, c.mood, c.timestamp),
+          )}
+        <Row size={1}>
+          <Column>
+            <Button title="Add Contact" onPress={this.selectContact} />
+          </Column>
+        </Row>
+        <Row size={1}>
+          <Column>
+            <Picker
+              title="My Mood"
+              native={true}
+              onSelect={e => this.setMood(e.native)}
+            />
+          </Column>
+        </Row>
+      </div>
+    )
   }
 }
